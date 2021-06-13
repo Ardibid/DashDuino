8d7a9632ec750b06fac6282fa27b5556253e581f
Ardavan Bidgoli <abidgoli@andrew.cmu.edu>
2021-06-13 16:10:27 -0400

    formatting code, adding docstrings

--- DashDuino_dbc_app.py
+++ DashDuino_dbc_app.py
@@ -8,28 +8,50 @@
     License: MIT
 '''
 
+######################################################################
+### Imports
+######################################################################
 import dash
-from dash_html_components.Hr import Hr
 import dash_daq as daq
 import dash_bootstrap_components as dbc
-
-import dash_html_components as html
-from dash.dependencies import Input, Output
 import dash_core_components as dcc
+import dash_html_components as html
 
+from dash.dependencies import Input, Output
+from dash_html_components.Hr import Hr
 
 import serial
 import json
 
+######################################################################
+### Variables
+######################################################################
+serial_port = None
+knob_values = {}
+counter = 0
+timeout_val = 100
+baudrate_val=115200
+port = 'COM4'
+
+
+######################################################################
+######################################################################
+### Dash App Begins Here
+######################################################################
+######################################################################
+
+
+######################################################################
+### Dash App setup
+######################################################################
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-serial_port = None
-knob_values = {}
-counter = 0
 
-
+######################################################################
+### Dash App components
+######################################################################
 setup_row = dbc.Row([
                     dbc.Col(html.H1("Arduino Dashboard")),
                     ])
@@ -96,11 +118,13 @@ gauges = html.Div([
                             html.Hr(),
                             ])
 ])
+
 controls = dbc.Row([
                     dbc.Card([
                          dbc.CardBody([
                                         html.H4("Setup", className="card-title"),
-                                        html.H6("Control the communicatino with Arduino", className="card-subtitle"),
+                                        html.H6("Control the communicatino with Arduino", 
+                                                className="card-subtitle"),
                                         html.Hr(),
                                         dbc.Button("Open port", id="port_controller"),
                                         html.P("No port", id="port_stat"),
@@ -115,6 +139,9 @@ controls = dbc.Row([
                             ])
                     ],className="mb-3",)
 
+######################################################################
+### Dash App Layout
+######################################################################
 app.layout = dbc.Container([setup_row,
                             html.Hr(),
                             gauges,
@@ -122,28 +149,38 @@ app.layout = dbc.Container([setup_row,
                             controls
                             ])
 
-
+######################################################################
+### Dash App Callbacks
+######################################################################
 @app.callback(
     Output(component_id='port_stat', component_property='children'),
     Output(component_id='port_controller', component_property='children'),
     Input(component_id='port_controller', component_property='n_clicks')
     )
 def port_manager(val):
+    """
+    Manages the serial port
+    At the begining, opens the port automatically
+    Upon further clicks, closes or opens the port and updates the notes
+    """
     global serial_port
-    print (val)
+    global timeout_val  
+    global baudrate_val 
+    global port  
+
+    # flip the port open and close on click
     if val:
         if val % 2 == 1 :
             serial_port.close()
             return (["Port is closed","Open Port"])
         else:
-            serial_port = serial.Serial('COM4',baudrate=115200, timeout=100)
+            serial_port = serial.Serial(port,baudrate=baudrate_val, timeout=timeout_val)
             return (["Port is open","Close Port"])
+    # opening the port on page load
     else:
-        serial_port = serial.Serial('COM4',baudrate=115200, timeout=100)
-        print (serial_port)
+        serial_port = serial.Serial(port, baudrate=baudrate_val, timeout=timeout_val)
         return (["Port is open","Close Port"])
-    # else:
-    #     return [dash.no_update, dash.no_update]
+
 
 @app.callback(
     Output(component_id='serial_val', component_property='children'),
@@ -156,35 +193,50 @@ def port_manager(val):
     Input(component_id="interval_component", component_property="n_intervals"),
     )
 def update_serila(interavl):
+    """
+    Reads the serial port data in given time intervals
+    The time interval is defined by the interval val in:
+                                dcc.Interval(
+                                            id='interval_component',
+                                            interval= 50)
+    Args:
+        interval: number of interval passed since the load, coming from
+                     dcc.Interval and its n_intervals value
+    """
     global serial_port
     global knob_values
     global counter
-    if serial_port :
-        try:
-            data_to_read = serial_port.inWaiting()
-            serial_msg = serial_port.read(data_to_read).decode('ascii')
-            msg = serial_msg.split("\r\n")[-2]
-            knob_values = json.loads(msg)
 
-            msg = ""
-            for key, value in knob_values.items():
-                msg = msg+ "{}: {}, ".format(key[-2:], value)
+    # checks if the serial port exists
+    if serial_port:
+        # checks if the serial port is open
+        if serial_port.isOpen():
+            try:
+                data_to_read = serial_port.inWaiting()
+                serial_msg = serial_port.read(data_to_read).decode('ascii')
+                msg = serial_msg.split("\r\n")[-2]
+                knob_values = json.loads(msg)
 
-            return ([msg, 
-                    knob_values["knob_00"],
-                    knob_values["knob_01"],
-                    knob_values["knob_02"],
-                    knob_values["knob_03"],
-                    knob_values["knob_04"],
-                    knob_values["knob_05"],])
-        except:
-            counter += 1
-            print ("had issues!",counter)
-            return (7*[dash.no_update])
+                msg = ""
+                for key, value in knob_values.items():
+                    msg = msg+ "{}: {}, ".format(key[-2:], value)
 
+                return ([msg, 
+                        knob_values["knob_00"],
+                        knob_values["knob_01"],
+                        knob_values["knob_02"],
+                        knob_values["knob_03"],
+                        knob_values["knob_04"],
+                        knob_values["knob_05"],])
+            except:
+                counter += 1
+                print ("had issues!",counter)
+                return (7*[dash.no_update])
+    # if port doesn't exist or is closed
+    return (["Still Nothing", 0, 0, 0, 0, 0, 0])
 
-    return (["Still Nothing", 0, 0, 0, 0, 0,0])
-
-
+######################################################################
+### Dash App Running!
+######################################################################
 if __name__ == '__main__':
     app.run_server(debug=True)
